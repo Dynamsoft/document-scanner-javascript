42e45169fed166971a954bd816595f18b51191f0
Tom Kent <Tom-Dynamsoft@users.noreply.github.com>
2025-01-06 11:15:38 -0800

    feat: update the structure to put files in the root dir

--- mobile-web-capture/src/mwc.bundle.esm.ts
+++
@@ -1,3 +0,0 @@
-export * from "dynamsoft-document-scanner";
-export * from "dynamsoft-document-viewer";
-export * from "./index";
--- mobile-web-capture/src/mwc.bundle.ts
+++
@@ -1,3 +0,0 @@
-export * as DDS from "dynamsoft-document-scanner";
-export * as DDV from "dynamsoft-document-viewer";
-export * from "./index";
