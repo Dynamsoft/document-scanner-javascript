ff84cee6846400c10ace9bcb5269d9fccb04331e
felixindynamsoft <indrawan.felix123@gmail.com>
2025-01-08 13:01:51 -0800

    refactor: samples and serving sample pages

---
+++ README.md
@@ -0,0 +1,23 @@
+## Running the sample
+
+We make use of the web server that comes with [Express](https://expressjs.com/).
+
+* Install dependencies
+
+Run the following script to install all dependencies.
+
+`npm run build`
+
+`npm install`
+
+* Start the server
+
+We defined the web server logic in the file "index.js". To start it, run the following script:
+
+`npm run serve`
+
+Note that we have used self-signed certificates to enable SSL on the port 3000.
+
+Additionally, you're able to access the application through mobile/network access by using the Network URL shown in the console.
+
+Make sure your device is on the same network as the server.
--- samples/index.js
+++ samples/index.js
@@ -1,11 +1,26 @@
-const express = require("express");
-const fs = require("fs");
-const https = require("https");
-const cors = require("cors");
-const path = require("path");
+import express from "express";
+import fs from "fs";
+import https from "https";
+import cors from "cors";
+import path from "path";
+import os from "os";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Check if dist folder exists
+const distPath = path.join(__dirname, "../dist");
+if (!fs.existsSync(distPath)) {
+  console.error("\x1b[31m%s\x1b[0m", "Error: 'dist' folder not found!");
+  console.log("\nPlease build the project first by running:");
+  console.log("\x1b[33m%s\x1b[0m", "npm run build");
+  console.log("\nThen try running the server again.\n");
+  process.exit(1);
+}
 
 const app = express();
-// Access-Control-Allow-Origin: **any**
+
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -14,7 +29,6 @@ app.use(
   })
 );
 
-// Redirect all HTTP to HTTPS
 app.use((req, res, next) => {
   if (!req.secure) {
     return res.redirect(["https://", req.get("Host"), req.url].join(""));
@@ -22,20 +36,22 @@ app.use((req, res, next) => {
   next();
 });
 
+// Serve static files
+app.use("/dist", express.static(distPath));
+
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/demo.html"));
+  res.sendFile(path.join(__dirname, "/demo.html"));
 });
 
 app.get("/demo", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/demo.html"));
+  res.sendFile(path.join(__dirname, "/demo.html"));
 });
 
 app.get("/hello-world", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/hello-world.html"));
+  res.sendFile(path.join(__dirname, "/hello-world.html"));
 });
 
-// Create HTTPS server
 const httpsServer = https.createServer(
   {
     key: fs.readFileSync(path.join(__dirname, "pem/key.pem")),
@@ -47,22 +63,27 @@ const httpsServer = https.createServer(
 let httpsPort = 3000;
 
 httpsServer.listen(httpsPort, "0.0.0.0", () => {
-  const networkInterfaces = require("os").networkInterfaces();
+  const networkInterfaces = os.networkInterfaces();
   let localIP;
   Object.keys(networkInterfaces).forEach((interfaceName) => {
-    networkInterfaces[interfaceName].forEach((interface) => {
-      if (interface.family === "IPv4" && !interface.internal) {
-        localIP = interface.address;
+    networkInterfaces[interfaceName].forEach((iface) => {
+      if (iface.family === "IPv4" && !iface.internal) {
+        localIP = iface.address;
       }
     });
   });
 
-  console.log("Dynamsoft Document Scanner Sample is available at");
-  console.log(`Local: https://localhost:${httpsPort}/`);
-  console.log(`Network: https://${localIP}:${httpsPort}/`);
-  console.log("-------------------");
-  console.log("Available pages:");
-  console.log("Demo Page: /demo");
-  console.log("Hello World Page: /hello-world");
-  console.log("Press Ctrl+C to stop the server");
+  console.log("\n\x1b[1m Dynamsoft Document Scanner Sample\x1b[0m\n");
+
+  console.log("\x1b[36m Access URLs:\x1b[0m");
+  console.log("\x1b[90m-------------------\x1b[0m");
+  console.log("\x1b[32m Local:\x1b[0m    https://localhost:" + httpsPort + "/");
+  console.log("\x1b[32m Network:\x1b[0m  https://" + localIP + ":" + httpsPort + "/\n");
+
+  console.log("\x1b[36m Available Pages:\x1b[0m");
+  console.log("\x1b[90m-------------------\x1b[0m");
+  console.log("\x1b[33m Demo:\x1b[0m        /demo");
+  console.log("\x1b[33m Hello World:\x1b[0m  /hello-world\n");
+
+  console.log("\x1b[90mPress Ctrl+C to stop the server\x1b[0m\n");
 });
