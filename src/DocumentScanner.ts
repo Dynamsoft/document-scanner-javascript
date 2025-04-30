d7ed940bde784c2fb4c664aa0f13c85ab9c9425a
felixindynamsoft <indrawan.felix123@gmail.com>
2025-04-30 14:20:07 -0700

    feat: update version to 1.2.0

--- src/DocumentScanner.ts
+++ src/DocumentScanner.ts
@@ -18,8 +18,7 @@ import { getElement, isEmptyObject, shouldCorrectImage } from "./views/utils";
 import { showLoadingScreen } from "./views/utils/LoadingScreen";
 
 // Default DCE UI path
-const DEFAULT_DCE_UI_PATH =
-  "https://cdn.jsdelivr.net/npm/dynamsoft-document-scanner@1.2.0-beta-1745342777464/dist/document-scanner.ui.html";
+const DEFAULT_DCE_UI_PATH = "https://cdn.jsdelivr.net/npm/dynamsoft-document-scanner@1.2.0/document-scanner.ui.html";
 const DEFAULT_DCV_ENGINE_RESOURCE_PATHS = { rootDirectory: "https://cdn.jsdelivr.net/npm/" };
 const DEFAULT_CONTAINER_HEIGHT = "100dvh";
 
