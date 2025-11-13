6c03c452c0b835d7d7a3568b1efead4d0451c45d
dynamsoft-h <172081603+dynamsoft-h@users.noreply.github.com>
2025-11-12 17:41:14 -0800

    feat: minimal DDV + MDS sample

--- dev-server/index.js
+++ dev-server/index.js
@@ -54,6 +54,10 @@ app.get("/continuous-scanning", (req, res) => {
   res.sendFile(path.join(__dirname, "../samples/scenarios/continuous-scanning.html"));
 });
 
+app.get("/ddv-scanner", (req, res) => {
+  res.sendFile(path.join(__dirname, "../samples/scenarios/ddv-scanner.html"));
+});
+
 app.get("/scenarios/use-file-input", (req, res) => {
   res.sendFile(path.join(__dirname, "../samples/scenarios/use-file-input.html"));
 });
@@ -168,6 +172,7 @@ httpServer.listen(httpPort, () => {
   console.log("\x1b[33m Hello World:\x1b[0m    http://localhost:" + httpPort + "/hello-world");
   console.log("\x1b[33m Demo:\x1b[0m    http://localhost:" + httpPort + "/demo");
   console.log("\x1b[33m Continuous Scanning:\x1b[0m    http://localhost:" + httpPort + "/continuous-scanning");
+  console.log("\x1b[33m Continuous Scanning:\x1b[0m    http://localhost:" + httpPort + "/ddv-scanner");
 });
 
 httpsServer.listen(httpsPort, "0.0.0.0", () => {
@@ -189,6 +194,7 @@ httpsServer.listen(httpsPort, "0.0.0.0", () => {
     console.log("\x1b[32m Hello World:\x1b[0m  https://" + localIP + ":" + httpsPort + "/hello-world");
     console.log("\x1b[32m Demo:\x1b[0m  https://" + localIP + ":" + httpsPort + "/demo");
     console.log("\x1b[32m Continuous Scanning:\x1b[0m  https://" + localIP + ":" + httpsPort + "/continuous-scanning");
+    console.log("\x1b[32m Continuous Scanning:\x1b[0m  https://" + localIP + ":" + httpsPort + "/ddv-scanner");
   });
   console.log("\n");
   console.log("\x1b[90mPress Ctrl+C to stop the server\x1b[0m\n");
